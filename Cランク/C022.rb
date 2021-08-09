e7328556f50952fc8a7a85f45b7919d22a98a442
katou02 <56467807+katou02@users.noreply.github.com>
2021-08-09 15:50:17 +0900

    Update C022.rb

--- Cランク/C022.rb
+++ Cランク/C022.rb
@@ -17,8 +17,7 @@ start = array_2.transpose[0][0].to_i
 goal = array_2.transpose[1][-1].to_i
 
 array_3 = []
-array_3.push(array_2.transpose[2])
-array_3.push(array_2.transpose[3])
+array_3.push(array_2.transpose[2],array_2.transpose[3])
 
 array_3.flatten!
 array_3.map!(&:to_i)
