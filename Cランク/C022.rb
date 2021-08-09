8cd4134f6b97899fb976ee38ca786b4d45a4822c
katou02 <56467807+katou02@users.noreply.github.com>
2021-08-09 15:49:00 +0900

    Create C022.rb

---
+++ Cランク/C022.rb
@@ -0,0 +1,29 @@
+day = gets.to_i
+array = []
+
+day.times do
+  array.push(gets.chomp)
+end
+
+n = array.length
+array_2 = [] 
+
+n.times do |m|
+  array_2.push(array[m].split)
+end
+
+
+start = array_2.transpose[0][0].to_i
+goal = array_2.transpose[1][-1].to_i
+
+array_3 = []
+array_3.push(array_2.transpose[2])
+array_3.push(array_2.transpose[3])
+
+array_3.flatten!
+array_3.map!(&:to_i)
+
+max = array_3.max
+min = array_3.min
+
+print("#{start} ","#{goal} ","#{max} ", "#{min}")
