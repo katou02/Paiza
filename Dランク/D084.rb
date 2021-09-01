e345f15d960c3927a4f3c67d54e30b2b9c3c0540
katou02 <56467807+katou02@users.noreply.github.com>
2021-09-01 22:52:12 +0900

    Create D084.rb

---
+++ Dランク/D084.rb
@@ -0,0 +1,27 @@
+month = gets.to_i
+
+if month == 1
+  puts "January"
+elsif month == 2
+  puts "February"
+elsif month == 3
+  puts "March"
+elsif month == 4
+  puts "April"
+elsif month == 5
+  puts "May"
+elsif month == 6
+  puts "June"
+elsif month == 7
+  puts "July"
+elsif month == 8
+  puts "August"
+elsif month == 9
+  puts "September"
+elsif month == 10
+  puts "October"
+elsif month == 11
+  puts "November"
+else
+  puts "December"
+end
