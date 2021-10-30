c651928c496e760f29b0cdfd334237ef715f913b
katou02 <56467807+katou02@users.noreply.github.com>
2021-10-31 00:51:17 +0900

    Create D048.rb

---
+++ Dランク/D048.rb
@@ -0,0 +1,10 @@
+array = []
+5.times do
+    a = gets.to_i
+     array.push(a)
+end
+
+puts (array[0] - array[1]).abs
+puts (array[1] - array[2]).abs
+puts (array[2] - array[3]).abs
+puts (array[3] - array[4]).abs
